634972022a78f1e7fb62e3c3c77b1cb7a613771a
BurakSenol00 <buraksenol00@outlook.com>
2025-03-04 21:03:57 +0300

    update button was activated, checks were made, transition to form 2 was made

--- FRMMhrsApp/Models/Bolum.cs
+++ FRMMhrsApp/Models/Bolum.cs
@@ -2,14 +2,45 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace FRMMhrsApp.Models
 {
     public class Bolum
     {
-        public string Adi { get; set; }
-        public string Aciklama { get; set; }
+        private string _adi;
+
+        public string Adi
+        {
+            get { return _adi; }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-ZĞÜŞİÖÇğüşıöç]+$"))
+                {
+                    MessageBox.Show("Sadece Harflerden Oluşmalıdır", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+               _adi = value;
+            }            
+        }
+        private string _aciklama;
+
+        public string Aciklama
+        {
+            get { return _aciklama; }
+            set
+            {
+                if (!Regex.IsMatch(value, @"^[a-zA-ZĞÜŞİÖÇğüşıöç]+$"))
+                {
+                    MessageBox.Show("Sadece Harflerden Oluşmalıdır", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+                
+                _aciklama = value;
+            }
+        }
         public override string ToString()
         {
             return Adi;
